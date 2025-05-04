699e202d2ded303806c5085052107d8218f92b83
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2025-05-04 01:45:38 +0100

    fixed merge conflict

--- referlut-frontend/src/components/Header.tsx
+++ referlut-frontend/src/components/Header.tsx
@@ -33,7 +33,6 @@ const Header = () => {
           >
             Home
           </Link>
-<<<<<<< HEAD
           <a
             href="#features"
             className="text-gray-700 hover:text-referlut-purple transition"
@@ -52,31 +51,6 @@ const Header = () => {
           >
             Testimonials
           </a>
-=======
-            {/* Check if user is on landing page */}
-            {location.pathname === "/" && (
-            <>
-              <Link
-              to="#features"
-              className="text-gray-700 hover:text-referlut-purple transition"
-              >
-              Features
-              </Link>
-              <Link
-              to="#how-it-works"
-              className="text-gray-700 hover:text-referlut-purple transition"
-              >
-              How It Works
-              </Link>
-              <Link
-              to="#testimonials"
-              className="text-gray-700 hover:text-referlut-purple transition"
-              >
-              Testimonials
-              </Link>
-            </>
-            )}
->>>>>>> refs/remotes/origin/main
         </nav>
 
         {/* Login/Signup/Profile Buttons */}
